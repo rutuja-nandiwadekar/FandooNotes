2b5bd49cb255df3987dc8fbf65370728e59d79b9
Rutuja <nandiwadekarrutuja@gmail.com>
2023-04-16 22:06:59 +0530

    Added : Welcome message

---
+++ src/main/java/com/bridgelabz/fandoo/FandooApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.fandoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FandooApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Fandoo application !!");
+		SpringApplication.run(FandooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/fandoo/FandooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.fandoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FandooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
